f2af554ec6faddf3aa4c337595d25f45e08fe988
MartijnGosgens <martijn.gosgens@hotmail.com>
2017-06-05 01:35:00 +0200

    Updated lifetime analysis

--- R scripts/LifetimeAnalysis.R
+++ R scripts/LifetimeAnalysis.R
@@ -1,11 +1,12 @@
 library(fitdistrplus)
 library(logspline)
 library(MASS) 
+library(ADGofTest)
 
 # Load data
 setwd('C:/Users/s147569/Dropbox/School/BEP/Plugins/R scripts')
 CsvData <-read.csv("TraceLifetimes.csv", header=TRUE)
-lifetimes <- CsvData$Lifetime
+lifetimes <- CsvData$Lifetime / (3600*1000) # In hours
 
 # Compute
 average <-mean(lifetimes)
@@ -17,12 +18,24 @@ shapiro <- shapiro.test(lifetimes)
 
 descdist(lifetimes, discrete = FALSE)
 # It is in the beta area but the beta distribution has support [0,1]
-# Gamma is more likely
+# Gamma or Weibull is more likely
 
-distr <- fitdistr(0.00001*lifetimes,"gamma") 
-gammaTest <- ks.test(0.00001*lifetimes,"pgamma", 39.1476, 0.00824)
-weibullTest <- ks.test(0.00001*lifetimes, "pweibull", scale=6.04, shape=5069.89)
+# estimate parameters
+normalFit <- fitdistr(lifetimes, "normal")
+weibullFit <- fitdistr(lifetimes, "weibull")
+gammaFit <- fitdistr(lifetimes, "gamma")
+
+# Test GoF
+normalTest <-ad.test(lifetimes,pnorm, mean=average, sd=standardDeviation)
+gammaTest <- ad.test(lifetimes,pgamma,shape=gammaFit$estimate[["shape"]], rate = gammaFit$estimate[["rate"]])
+weibullTest <- ad.test(lifetimes, pweibull,shape=weibullFit$estimate[["shape"]], scale = weibullFit$estimate[["scale"]])
 
 # Plot
-plot(cdf,xlim=c(0,2*average))
 qqnorm(lifetimes); qqline(lifetimes)
+plot(cdf,xlim=c(0,2*average), col="green", ylab="F(x)",main="Observed (green) vs theoretical (red)")
+x <- seq(0,2*average,2*average/100)
+lines(x,pgamma(x,shape=gammaFit$estimate[["shape"]], rate = gammaFit$estimate[["rate"]]), col="red")
+#lines(x,pnorm(x,mean=average,sd=standardDeviation), col="blue")
+#lines(x,pweibull(x,shape=weibullFit$estimate[["shape"]], scale = weibullFit$estimate[["scale"]]), col="black")
+
+print(paste("Gamma with shape",gammaFit$estimate[["shape"]],"and rate",gammaFit$estimate[["rate"]],"p-value",gammaTest$p.value))
